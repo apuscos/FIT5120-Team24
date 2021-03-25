c4e270b18faf4b889167717c837ea0842bef18c8
Gao Wang <gwan58@student.monash.edu>
2021-03-25 17:17:53 +0800

    1. Add input box for search for agency eligibility, and the example call api function

--- root4all/src/Home.js
+++ root4all/src/Home.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components";
 
 
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
 
+const Input = styled.input`
+  font-size: 14px;
+  line-height: 1;
+  width: 100%;
+  border: solid black;
+`;
+
+async function callApi(inputVal, callback) {
+    console.log(inputVal)
+    callback("test")
+}
 
 function Home() {
+    const [input, setInput] = useState("");
+    const [result, setResult] = useState("");
     return (
         <div>
             This is home page
+            <Input onChange={e => setInput(e.target.value)} placeholder={"Please Enter Agency name"}/>
+            <Button onClick={() => callApi(input, setResult)}>Search</Button>
+            <div>{result}</div>
         </div>
 );
 }
