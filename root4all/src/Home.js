7d278b82089662406d72c6f15bc7ef04ee311905
Gao Wang <gwan58@student.monash.edu>
2021-03-25 15:56:52 +0800

    1. Create input box 2. The callApi function can receive the input from the input box

--- root4all/src/Home.js
+++ root4all/src/Home.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components";
+
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
+const Input = styled.input`
+  font-size: 14px;
+  line-height: 1;
+  width: 100%;
+  border: solid black;
+  
+  
+`;
+
+
+async function callApi(inputVal) {
+    console.log(inputVal)
+}
 
 function Home() {
-  return (
-    <div className="App">
-      This is home page
-    </div>
-  );
+    const [input, setInput] = useState("");
+    return (
+        <div>
+            <Input onChange={e => setInput(e.target.value)} />
+            <Button onClick={() => callApi(input)}>Search</Button>
+        </div>
+    );
 }
 
 export default Home;
