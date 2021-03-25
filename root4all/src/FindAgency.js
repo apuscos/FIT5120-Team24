77a0c5d8b5c77450a6d37455f60aad9316721f3c
Gao Wang <gwan58@student.monash.edu>
2021-03-25 16:37:27 +0800

    1. Create result area to display some testing data

--- root4all/src/FindAgency.js
+++ root4all/src/FindAgency.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
+const Input = styled.input`
+  font-size: 14px;
+  line-height: 1;
+  width: 100%;
+  border: solid black;
+`;
+
+async function callApi(inputVal, callback) {
+    console.log(inputVal)
+    callback(["test1", "test2", "test3"])
+}
 
 function FindAgency() {
+    const [input, setInput] = useState("");
+    const [result, setResult] = useState([]);
     return (
-        <div className="App">
-            This is find agency page
+        <div>
+            <Input onChange={e => setInput(e.target.value)}/>
+            <Button onClick={() => callApi(input, setResult)}>Search</Button>
+            <div>
+                <h3>Result:</h3>
+                <ul>
+                    {result.map((x,i) => {
+                            return (
+                                <li key={i}>{x}</li>
+                            )
+                        }
+                    )}
+                </ul>
+            </div>
         </div>
-    );
+    )
 }
 
 export default FindAgency;
--- root4all/src/Home.js
+++ root4all/src/Home.js
@@ -1,38 +1,14 @@
-import React, {useState} from 'react';
-import styled from "styled-components";
+import React from 'react';
 
 
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-`;
 
-const Input = styled.input`
-  font-size: 14px;
-  line-height: 1;
-  width: 100%;
-  border: solid black;
-  
-  
-`;
-
-
-async function callApi(inputVal) {
-    console.log(inputVal)
-}
 
 function Home() {
-    const [input, setInput] = useState("");
     return (
         <div>
-            <Input onChange={e => setInput(e.target.value)} />
-            <Button onClick={() => callApi(input)}>Search</Button>
+            This is home page
         </div>
-    );
+);
 }
 
 export default Home;
