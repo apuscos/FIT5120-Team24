0cfb04cd5ea552b3920f931eacf3532647c3c411
Gao Wang <gwan58@student.monash.edu>
2021-04-01 12:03:55 +0800

    1. Update

--- root4all/src/About.js
+++ root4all/src/About.js
@@ -1,58 +1,109 @@
 import React from 'react';
 import styled from "styled-components";
 import {Logo} from "./Navigation/NavComponents"
+import backgroundImg from "./Image/aboutBackground.png"
+import teamImg from "./Image/teamImage.png"
 
 const ProductDesc = styled.div`
-  height: 300px;
-  background-color: #e3e3e3;
   display: flex;
-  justify-content: center;
+  padding-left: 5%;
   padding-top: 20px;
   flex-direction: column;
-  align-items: center;
+  justify-content: center;
+  
+  
 `;
 
 const TeamDesc = styled.div`
-  height: 300px;
-  background-color: #d7d6d6;
+  height: 700px;
+  background-color: white;
   display: flex;
   justify-content: center;
   padding-top: 20px;
   flex-direction: column;
   align-items: center;
+  
 `;
 
 const Title = styled.div`
   font-family: "Bebas Neue", cursive;
-  font-size: 2em;
+  font-size: 3em;
   width: auto;
-  text-align: center;
   margin-bottom: 10px;
+  
+  
 `;
 
 const Content = styled.div`
   font-family: 'Lato', sans-serif;
-  width: 50%;
+  width: 40%;
   display: flex;
-  font-size: 1.2em;
+  font-size: 1.7em;
 `;
 
+const ContentBigger = styled.div`
+  font-family: 'Lato', sans-serif;
+  width: 90%;
+  display: flex;
+  font-size: 2em;
+`;
+
+const TitleBigger = styled.div`
+  font-family: "Bebas Neue", cursive;
+  font-size: 3em;
+  width: auto;
+  margin-bottom: 10px;
+`;
+const ImageSection = styled.div`
+  height: 350px;
+  width: 30%;
+  margin-top: 30px;
+  background-image: url(${backgroundImg});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
+
+const AreaWarpper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 400px;
+  padding-left: 20%;
+  background-color: #979696;
+`;
+
+const TeamImg = styled.div`
+  height: 400px;
+  margin-top: 50px;
+  width: 1674px;
+  background-image: url(${teamImg});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 1674px 400px;
+  margin-bottom: 50px;
+`;
 function About() {
     return (
-        <div className="App">
-            <ProductDesc>
-                <Logo></Logo>
-                <Content>
-                    Roof 4 All strives to provide a consolidated form of the integration of all necessary information on available social housing options aiding to the needs, the eligibility in availing certain housing services and other support options for the homeless or people at the risk of homelessness due to the difficult situations stemmed from the fallout of the Covid pandemic
-                </Content>
-            </ProductDesc>
+        <>
+            <AreaWarpper className="App">
+                <ImageSection/>
+                <ProductDesc>
+                    <Title>Roof4All</Title>
+                    <Content>
+                        Roof 4 All strives to provide a consolidated form of the integration of all necessary information on available social housing options aiding to the needs, the eligibility in availing certain housing services and other support options for the homeless or people at the risk of homelessness due to the difficult situations stemmed from the fallout of the Covid pandemic
+                    </Content>
+                </ProductDesc>
+            </AreaWarpper>
             <TeamDesc>
-                <Title>Team Description</Title>
-                <Content>
+                <TitleBigger>Meet the team</TitleBigger>
+                <ContentBigger>
                     We, The Remnants, are a team of graduate students from IT, Data Science, Business Information Systems and Network and Security and we aim to work collaboratively to help the homeless people
-                </Content>
+                </ContentBigger>
+                <TeamImg/>
             </TeamDesc>
-        </div>
+
+        </>
     );
 }
 
