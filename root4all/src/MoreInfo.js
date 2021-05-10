8fbec96e5595a2b1e10757b48c4addb5ea35fd6c
Gao Wang <gwan58@student.monash.edu>
2021-05-10 19:34:49 +0800

    Add Housing Support for the Aged Program services section

--- root4all/src/MoreInfo.js
+++ root4all/src/MoreInfo.js
@@ -168,6 +168,192 @@ function MoreInfo(){
                     </Table>
                 </TableContainerStyled>
             </TableWrapper>
+            <TableWrapper>
+                <Typography variant={"h4"}>Housing Support for the Aged Program services</Typography>
+                <TableContainerStyled component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <StyledTableCell align={"left"}><Typography variant={"h6"}>DHHS Division</Typography></StyledTableCell>
+                                <StyledTableCell align={"left"}><Typography variant={"h6"}>Service provider</Typography></StyledTableCell>
+                                <StyledTableCell align={"left"}><Typography variant={"h6"}>Phone</Typography></StyledTableCell>
+                                <StyledTableCell align={"left"}><Typography variant={"h6"}>Address</Typography></StyledTableCell>
+
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>South</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Star Health</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 9525 1300</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>240 Malvern Road, Prahran VIC 3181</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>South</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>ERMHA Pathways Cluster</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 9706 7388 </Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>45 Assembly Drive, Dandenong VIC 3175</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>South</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Quantum Support Services</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 5120 2165</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>227 Princes Drive, Morwell VIC 3840</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>North</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Merri Outreach Support Services</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 9380 6036</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>351 Barkly Street, Brunswick VIC 3056</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>North</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Haven; Home, Safe</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 5444 9044 </Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>10-16 Forest Street, Bendigo VIC 3550</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>West</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Cohealth</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 9448 5510 </Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>215 Nicholson Street, Footscray VIC 3011</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>West</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Salvation Army Adult Services</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 9328 5631</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Mail Box 62 /159 Melrose Street, North Melbourne VIC 3051</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>West</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Centacare Ballarat</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 5337 8999</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>4-6 Peel Street, Ballarat VIC 3350</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>West</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Wintringham</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 9375 3774</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>136 Mt Alexander Road, Flemington VIC 3031</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>West</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Genu- Karringal St Laurence</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 5241 0685</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>117 Pakingston Street, Geelong West VIC 3128</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>East</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>SalvoCare</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>+61 3 5820 8000</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>2-8 McLennan Street, Mooroopna VIC 3629</Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow key={"single"}>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>East</Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>Uniting </Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>1300 277 478 </Typography>
+                                </TableCell>
+                                <TableCell component="th" scope="row" >
+                                    <Typography>321 Ferntree Gully Road, Mt Waverley VIC 3149</Typography>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainerStyled>
+            </TableWrapper>
         </>
     )
 }
